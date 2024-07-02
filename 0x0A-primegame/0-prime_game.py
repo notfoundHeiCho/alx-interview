f206a7cd86c73dfc695e538e575e4b547b68044b
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-07-03 00:45:11 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,42 @@
 #!/usr/bin/python3
-"""
-Prime Game Algorithm Python
-"""
-
-
-def primeNumbers(n):
-    """this function return an array of prime numbers
-    smaller or equal to n using Sieve of Eratosthenes"""
-    result = []
-    prime = [True for i in range(n+1)]
-    p = 2
-    while (p * p <= n):
-        if (prime[p] is True):
-            for i in range(p * p, n+1, p):
-                prime[i] = False
-        p += 1
-    for p in range(2, n+1):
-        if prime[p]:
-            result.append(p)
-    return result
+"""0. Prime Game - Maria and Ben are playing a game"""
 
 
 def isWinner(x, nums):
-    """Maria and Ben are playing a game.
-    Given a set of consecutive integers starting from 1 up to and including n,
-    they take turns choosing a prime number from the set and removing
-    that number and its multiples from the set.
-    The player that cannot make a move loses the game.
-    They play x rounds of the game,
-    where n may be different for each round.
-    Assuming Maria always goes first and both players play optimally,
-    determine who the winner of each game is.
+    """x - rounds
+    nums - numbers list
     """
-
+    if x <= 0 or nums is None:
+        return None
     if x != len(nums):
         return None
 
-    Ben = 0
-    Maria = 0
+    ben = 0
+    maria = 0
 
-    for num in nums:
-        primes = primeNumbers(num)
-        if len(primes) % 2 == 0:
-            Ben += 1
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            Maria += 1
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
+    return None
 
-    return "Ben" if Ben > Maria else "Maria"
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
